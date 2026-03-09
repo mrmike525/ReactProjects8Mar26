d163436a610d163eb8e0252247cd501c56ca90f9
Michael Lange <mrlange892@gmail.com>
2026-03-09 18:47:58 -0500

    Update App.jsx

--- ProjectAlpha/src/App.jsx
+++ ProjectAlpha/src/App.jsx
@@ -3,14 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+function changeColor(){
+  const one = document.getElementById('one');
+  const taco = document.getElementById('taco');
+  if(one.checked == false){
+    taco.style.color = "blue";
+  } else {
+    taco.style.color = "yellow";
+  }
+};
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>HELP ME! SOMEBODY HELP ME PLEASE!!! mucho</p>
+      <ul>
+        
+        <li id="taco"> <span style={{backgroundColor: "black", fontSize: "50px"}}> <input id="one" type="checkbox" onClick={changeColor}  /> Taco Bout it</span></li>
+      </ul>
     </>
   )
 }
 
 export default App
+
+
+{/* <h3 style={{ color: quantity <= 5 ? "hsla(14, 78%, 55%, 1.00)" : "black" }} */}
