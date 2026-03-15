a854309a6b45f5af1c5a00bc3dfe432eb69d8924
Michael Lange <mrlange892@gmail.com>
2026-03-15 10:53:31 -0500

    latest sync

--- ProjectAlpha/src/App.jsx
+++ ProjectAlpha/src/App.jsx
@@ -3,8 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Chakra from './Chakra'
-
+import { changeColor } from './changeColor'
 const colors = {Crown: "magenta", ThirdEye: "purple", Throat: "blue", Heart: "green", SolarPlexus: "yellow", Navel: "orange", Root: "red"};
+
+let value1 = false
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,11 +17,16 @@ function App() {
     <Chakra color = {colors.Crown}/>
     <Chakra color = {colors.ThirdEye}/>
     <Chakra color = {colors.Throat}/>
-    <Chakra color = {colors.Heart}/>
+    <Chakra color = {colors.Heart}>
+      <span>Well this is the way! Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim culpa quidem, molestias commodi quae non, corrupti ex nulla delectus placeat eaque magnam! Pariatur facilis rerum quis fuga beatae, architecto vel?</span>
+      <p>help</p>
+      <button onClick={value1 = true} style={{color: value1 === true ? "green": "red"}}>Change Color </button>
+      </Chakra>
     <Chakra color = {colors.SolarPlexus}/>
     <Chakra color = {colors.Navel}/>
     <Chakra color = {colors.Root}/>
      </div>
+     <p>help</p>
     </>
   )
 }
--- ProjectAlpha/src/Chakra.jsx
+++ ProjectAlpha/src/Chakra.jsx
@@ -1,9 +1,17 @@
 
 
-const Chakra = function({color}) {
+
+const Chakra = function({color, children}) {
     console.log(color);
-    
-    
-    return <div style={{backgroundColor: color, width: "200px", height: "200px", alignContent: "center"}}></div>
+    let greenWidth = "200px"
+    if(color === "green"){
+        greenWidth = "600px"
+    }
+    return <div style={{backgroundColor: color, width: greenWidth, height: "200px", alignContent: "center"}}>
+        {children}
+    </div>
 };
 export default Chakra;
+
+// style= {{backgroundColor color === "green" ? "orange" : "green"}}
+
