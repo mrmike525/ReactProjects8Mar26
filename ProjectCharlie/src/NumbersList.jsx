36922b1056e952d74b36c9e39424d72dc88ece8f
mrmike525 <mrlange892@gmail.com>
2026-03-17 11:14:10 -0500

    latest

--- ProjectCharlie/src/NumbersList.jsx
+++ ProjectCharlie/src/NumbersList.jsx
@@ -4,13 +4,13 @@ const NumbersList = () => {
     const [numbers, setNumbers] = useState([2, 5, 7, 11, 12, 18]);
     const remove = (num) => {
         setNumbers(numbers.filter(n => n !== num))
-        console.log("REMOVING:", n);
+        console.log("REMOVING:", num);
     }
     return (
         <ul>
             {numbers.map(n => (
-                <li>
-                    <button onClick={() => remove(n)}>{n}</button>
+                <li key={n}>
+                    <button  onClick={() => remove(n)}>{n}</button>
                 </li>
             ))}
         </ul>
