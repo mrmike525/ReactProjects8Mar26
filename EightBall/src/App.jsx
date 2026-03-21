f80bd8089364114b0d8eb9ca390f4f5e8fc3c991
mrmike525 <mrlange892@gmail.com>
2026-03-21 02:38:20 -0500

    changes

---
+++ EightBall/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import EightBall from "./EightBall";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <EightBall />
+    </>
+  );
+}
+
+export default App;
---
+++ EightBall/src/EightBall.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+function EightBall() {
+  const [state, setState] = useState();
+  return <div>help</div>;
+}
+export default EightBall;
