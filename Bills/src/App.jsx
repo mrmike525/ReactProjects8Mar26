1f30795bfb9ebe517675f16d72c215819a0323d7
mrmike525 <mrlange892@gmail.com>
2026-03-26 07:07:39 -0500

    changes

--- Bills/src/App.jsx
+++ Bills/src/App.jsx
@@ -3,10 +3,21 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const advanceCount = () => setCount((c) => {return c + 1})
+const Alpha = [
+  'one', 'two', 'three', 'four', 'five'
+]
   return (
     <>
+      <div className='state' onClick={() => advanceCount()}>
+        {count}
       
+      <button className='advanceState'>Click To Advance The Count</button>
+    
+    </div>
+    {/* {Alpha.map((item, idx)=> (<button key={item}>{item}</button>
+    ))}   */}
+
     </>
   )
 }
