5ab2c0f5b3f1b12e889f627c0ae4dc42c64a38b8
Michael Lange <mrlange892@gmail.com>
2026-03-29 16:01:56 -0500

    changes

--- Bills/src/App.jsx
+++ Bills/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Table from './Table'
+import Data from './Accounts'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Table object = {Data}/>
     </>
   )
 }
---
+++ Bills/src/function.js
@@ -0,0 +1,39 @@
+let add = (value = 4, value2 = 16) => value + value2; 
+let subtract = (value = 4, value2 = 16) => (value - value2) === -12 ? "THIS IS ODD": value - value2; 
+let multiply = (value = 4, value2 = 16) => value * value2; 
+let divide = (value = 4, value2 = 16) => value / value2; 
+
+
+let allFuncs = () => {console.log(add());
+console.log(subtract());
+console.log(multiply());
+console.log(divide());
+}
+
+allFuncs();
+
+const annon = [];
+annon.push('cherry');
+annon.push('taco');
+console.log(annon[1]);
+
+function test(...help){return help}
+console.log(test(1, 2, 3))
+
+const MinPaymentAndDate = ["$30, 4/8/24", "$0, 6/8/24", "$30.22, 3/8/25", "$30, 4/8/25", "$30, 6/8/25", "$49, 7/8/25", "$48, 8/8/25", "$47, 9/8/25", "$43, 10/8/25", "$61, 11/8/25", "$79, 12/8/25", "$81, 1/8/26", "$79, 2/8/26", "$73, 3/8/26"];
+
+
+function takesFour(...one) {
+  console.log(one);
+  
+}
+const newDiv = document.createElement('div');
+const toAppend = document.querySelector('body');
+toAppend.append(newDiv); 
+newDiv.style.width = "200px"
+newDiv.style.height = "200px"
+newDiv.style.backgroundColor = "orange"
+takesFour(...MinPaymentAndDate);
+MinPaymentAndDate.map(function(values){
+    newDiv.append(values);
+})
