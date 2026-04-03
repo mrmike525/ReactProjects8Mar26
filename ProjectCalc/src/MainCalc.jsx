1d28b4cf47cdc2b00b9838229bae090844c6fb67
Michael Lange <mrlange892@gmail.com>
2026-04-03 18:41:54 -0500

    del works

--- ProjectCalc/src/MainCalc.jsx
+++ ProjectCalc/src/MainCalc.jsx
@@ -1,16 +1,17 @@
 import RenderCalc from "./RenderCalc";
 function mainCalc(){
     const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const operators = ["➕", "➖", "✖️", "➗", "🟰"];
+    const operators = ["➕", "➖", "✖️", "➗", "🟰", "DEL"];
     function add(valueA, valueB){return valueA + valueB};
     function subtract(valueA, valueB){return valueA - valueB};
     function multiply(valueA, valueB){return valueA * valueB};
     function divide(valueA, valueB){return valueA / valueB};
+    function del(valueA){if(valueA === "DEL"){return 0}};
     return (
         <>
         
         {/* <h2>Hi</h2> */}
-        <RenderCalc nums={digits} operation={operators} add={add} subtract={subtract} multiply={multiply} divide={divide}/>
+        <RenderCalc nums={digits} operation={operators} add={add} subtract={subtract} multiply={multiply} divide={divide} del={del}/>
     </>
     )
 }
--- ProjectCalc/src/RenderCalc.jsx
+++ ProjectCalc/src/RenderCalc.jsx
@@ -1,6 +1,6 @@
 import './RenderCalc.css'
 import { useState } from 'react'
-function RenderCalc({nums, operation, add, subtract, multiply, divide}){
+function RenderCalc({nums, operation, add, subtract, multiply, divide, del}){
 const [result, setResult] = useState(0);
 //  console.log(nums)
 // console.log(add)
@@ -9,7 +9,7 @@ const [result, setResult] = useState(0);
         <div className='result'>{result}</div>
         <span className="buttonContainer">
         {nums.map(function(item){return <button className="RenderCalc" key={item} onClick={()=> setResult((r) => (add(r, item)))}> {item} </button>})}
-        {operation.map((item)=>(<button key={item}>{item}</button>))}
+        {operation.map((item)=>(<button key={item} onClick={()=>setResult(() => (del(item)))}>{item}</button>))}
     </span>
     </>
     )
