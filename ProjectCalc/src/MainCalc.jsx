1bafd70d70311f6a597956759c90db61a367f30d
mrmike525 <mrlange892@gmail.com>
2026-04-02 19:17:03 -0500

    latest changes

--- ProjectCalc/src/MainCalc.jsx
+++ ProjectCalc/src/MainCalc.jsx
@@ -1,7 +1,7 @@
 import RenderCalc from "./RenderCalc";
 function mainCalc(){
     const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const operators = ["+", "-", "*", "/", "="];
+    const operators = ["➕", "➖", "✖️", "➗", "🟰"];
     function add(valueA, valueB){return valueA + valueB};
     function subtract(valueA, valueB){return valueA - valueB};
     function multiply(valueA, valueB){return valueA * valueB};
@@ -9,7 +9,7 @@ function mainCalc(){
     return (
         <>
         
-        <h2>Hi</h2>
+        {/* <h2>Hi</h2> */}
         <RenderCalc nums={digits} operation={operators} add={add} subtract={subtract} multiply={multiply} divide={divide}/>
     </>
     )
