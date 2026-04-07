2cd0b0261d79d32bbeb8e49636756c6096c5192a
mrmike525 <mrlange892@gmail.com>
2026-04-07 18:16:29 -0500

    Update SpaceBattle.jsx

--- SpaceBattle/src/SpaceBattle.jsx
+++ SpaceBattle/src/SpaceBattle.jsx
@@ -3,23 +3,26 @@ import { useState } from "react";
 function SpaceBattle(){
    const [yourLife, setYourLife] = useState(100);
    const [enemysLife, setEnemysLife] = useState(100);
-
-   function randomDamage(){
-   const x = Math.floor(Math.random() * 100);
-   return x;
+    const [gameStatus, setGameStatus] = useState(null);
+   function randomDamage(range = 100){
+   const x = () => Math.floor(Math.random() * range);
+   setYourLife((value)=>{return value - x()});
+    setEnemysLife((value)=>{return value - x()});
+    if(setYourLife >= 0){
+        setGameStatus("YOU LOSE!! GAME OVER")
+    }
+    return x()
   }
-  let y = randomDamage();
-    console.log(randomDamage())
+    // console.log(randomDamage())
     return (
         <>
         <div className="buttonContainer">
-            <h3 style={{display: "inline"}}>Your Health<span style={{color: "green"}}>{yourLife}</span></h3>
-            <button onClick={()=>{setYourLife((value)=>{return value - randomDamage()});
-        setEnemysLife((value)=>{return value - randomDamage()})
-        }}>Click To Fire!</button>
-            <h3 style={{display: "inline"}}>Enemy's Health <span style={{color: "green"}}>{enemysLife}</span></h3>
+            <h3 style={{display: "inline"}}>Your Health <span style={{ color: yourLife >= 0? "green" :"red" }}> {yourLife}</span></h3>
+            <button onClick={()=>{randomDamage()}}>Click To Fire!</button>
+            <h3 style={{display: "inline"}}>Enemy's Health <span style={{color: enemysLife >= 0? "green" : "red"}}>{enemysLife}</span></h3>
         </div>
-        <div><button onClick={()=> (console.log(randomDamage()))}>Get Value</button></div>
+        <div><button onClick={()=> (console.log(randomDamage(80)))}>Get Value</button></div>
+        <div>{gameStatus}</div>
         </>
     )
 }
