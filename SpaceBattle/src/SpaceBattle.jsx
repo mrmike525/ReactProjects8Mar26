d0866c3cf608f2039b1635e23fbc08ac288bada0
mrmike525 <mrlange892@gmail.com>
2026-04-07 12:00:05 -0500

    update spaceBattle

--- SpaceBattle/src/SpaceBattle.jsx
+++ SpaceBattle/src/SpaceBattle.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function SpaceBattle(){
    const [yourLife, setYourLife] = useState(100);
-   const [enemysLife, setEnemysLife] =useState(100);
+   const [enemysLife, setEnemysLife] = useState(100);
 
    function randomDamage(){
    const x = Math.floor(Math.random() * 100);
@@ -11,13 +11,16 @@ function SpaceBattle(){
   let y = randomDamage();
     console.log(randomDamage())
     return (
+        <>
         <div className="buttonContainer">
-            {yourLife}
+            <h3 style={{display: "inline"}}>Your Health<span style={{color: "green"}}>{yourLife}</span></h3>
             <button onClick={()=>{setYourLife((value)=>{return value - randomDamage()});
         setEnemysLife((value)=>{return value - randomDamage()})
         }}>Click To Fire!</button>
-            {enemysLife}
+            <h3 style={{display: "inline"}}>Enemy's Health <span style={{color: "green"}}>{enemysLife}</span></h3>
         </div>
+        <div><button onClick={()=> (console.log(randomDamage()))}>Get Value</button></div>
+        </>
     )
 }
 
