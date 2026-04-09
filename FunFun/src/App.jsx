eb45d988f95b2ba4cbe851b15b204843fbf67d2b
mrmike525 <mrlange892@gmail.com>
2026-04-09 14:03:33 -0500

    dataStructure Changes

--- FunFun/src/App.jsx
+++ FunFun/src/App.jsx
@@ -3,38 +3,18 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
+  const missionCount = [1,2,3,4,5,6,7,8,9,10];
+  const missionName = ["Study asteroid","Setup Sun observation sattelite","Fly around the Moon","Setup Moon Base"," Mine minerals from the Moon","Jump on the Moon", "Enjoy the Sunset on the Moon", "Nuke the Moon", "Leave the moon", "Setup Farm on Mars", "Nuke test Mars" ]
+  const status = ["Planned", "Active", "Completed"]
+  const crew = ["Michael", "Abby", "Tom", "Tina", "Becca"]
   const missions = [
     {
-      id: "1 - 10",
-      name: "Study Asteroid, Setup Sun observation sattelite, Fly around the Moon, Setup Moon Base, Mine minerals from the Moon, Jump on the Moon, Enjoy the Sunset on the Moon, Nuke the Moon, Leave the moon, Setup Farm on Mars, Nuke test Mars",
-      status: "Planned, Active, Completed",
-      crew: "Michael, Abby, Tom, Tina, Becca"
-    },
-     {
       id: "",
-      name: "",
+      name:  "",
       status: "",
       crew: ""
     },
-     {
-      id: "",
-      name: "",
-      status: "",
-      crew: ""
-    },
-     {
-      id: "",
-      name: "",
-      status: "",
-      crew: ""
-    },
-     {
-      id: "",
-      name: "",
-      status: "",
-      crew: ""
-    },
-  ]
+     ]
 
   return (
     <>
@@ -43,6 +23,7 @@ function App() {
       <a href="">Get him to the Greek!</a>
       <a href="">Let's Taco BOUT IT</a>
      </div>
+     <div>Mission ID: {missionCount[0]} Mission Name: {missionName[0]} Mission Status: {status[0]} Crew: {crew[0] + " " + crew[1]} </div>
     </>
   )
 }
