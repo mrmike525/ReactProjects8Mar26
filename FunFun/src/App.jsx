680da0a32567710de9714dc6249a73d8c1fdabd6
mrmike525 <mrlange892@gmail.com>
2026-04-09 13:03:38 -0500

    funfun project (component)

--- FunFun/src/App.jsx
+++ FunFun/src/App.jsx
@@ -2,7 +2,39 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const missions = [
+    {
+      id: "1 - 10",
+      name: "Study Asteroid, Setup Sun observation sattelite, Fly around the Moon, Setup Moon Base, Mine minerals from the Moon, Jump on the Moon, Enjoy the Sunset on the Moon, Nuke the Moon, Leave the moon, Setup Farm on Mars, Nuke test Mars",
+      status: "Planned, Active, Completed",
+      crew: "Michael, Abby, Tom, Tina, Becca"
+    },
+     {
+      id: "",
+      name: "",
+      status: "",
+      crew: ""
+    },
+     {
+      id: "",
+      name: "",
+      status: "",
+      crew: ""
+    },
+     {
+      id: "",
+      name: "",
+      status: "",
+      crew: ""
+    },
+     {
+      id: "",
+      name: "",
+      status: "",
+      crew: ""
+    },
+  ]
 
   return (
     <>
