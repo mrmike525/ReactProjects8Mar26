5bc8e1b4fb4ff8f952f8b292764f88bf0f93e696
mrmike525 <mrlange892@gmail.com>
2026-04-15 12:38:14 -0500

    latest changes

--- FunFun/src/MissionControl.jsx
+++ FunFun/src/MissionControl.jsx
@@ -7,7 +7,7 @@ function MissionControl({missions}){
     
     return(
      missions.map(({id, name, status, crew})=>
-      {return <div className="CardContainer"> <MissionCard id={id} name={name} status={status} crew={crew}/> <MissionAction/></div>})
+      {return <div className="contentContainer"> <div className="CardContainer"> <MissionCard id={id} name={name} status={status} crew={crew}/> <MissionAction/></div></div>})
       )}
 
 export default MissionControl;
