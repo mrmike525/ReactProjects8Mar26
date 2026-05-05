14feabcffc5e8c7b98f5b87e1ecbaca30ba9c4ec
mrmike525 <mrlange892@gmail.com>
2026-05-05 02:58:30 -0500

    j

--- Jeopardy/jeopardy.js
+++ Jeopardy/jeopardy.js
@@ -1,77 +1,9 @@
-// You only need to touch comments with the todo of this file to complete the assignment!
-
-/*
-=== How to build on top of the starter code? ===
-
-Problems have multiple solutions.
-We have created a structure to help you on solving this problem.
-On top of the structure, we created a flow shaped via the below functions.
-We left descriptions, hints, and to-do sections in between.
-If you want to use this code, fill in the to-do sections.
-However, if you're going to solve this problem yourself in different ways, you can ignore this starter code.
- */
-
-/*
-=== Terminology for the API ===
-
-Clue: The name given to the structure that contains the question and the answer together.
-Category: The name given to the structure containing clues on the same topic.
- */
-
-/*
-=== Data Structure of Request the API Endpoints ===
-
-/categories:
-[
-  {
-    "id": <category ID>,
-    "title": <category name>,
-    "clues_count": <number of clues in the category where each clue has a question, an answer, and a value>
-  },
-  ... more categories
-]
-
-/category:
-{
-  "id": <category ID>,
-  "title": <category name>,
-  "clues_count": <number of clues in the category>,
-  "clues": [
-    {
-      "id": <clue ID>,
-      "answer": <answer to the question>,
-      "question": <question>,
-      "value": <value of the question (be careful not all questions have values) (Hint: you can assign your own value such as 200 or skip)>,
-      ... more properties
-    },
-    ... more clues
-  ]
-}
- */
-
 const API_URL = "https://rithm-jeopardy.herokuapp.com/api/"; // The URL of the API.
 const NUMBER_OF_CATEGORIES = 6; // The number of categories you will be fetching. You can change this number.
 const NUMBER_OF_CLUES_PER_CATEGORY = 5; // The number of clues you will be displaying per category. You can change this number.
 
 let categories = []; // The categories with clues fetched from the API.
-/*
-[
-  {
-    "id": <category ID>,
-    "title": <category name>,
-    "clues": [
-      {
-        "id": <clue ID>,
-        "value": <value (e.g. $200)>,
-        "question": <question>,
-        "answer": <answer>
-      },
-      ... more categories
-    ]
-  },
-  ... more categories
-]
- */
+
 
 let activeClue = null; // Currently selected clue data.
 let activeClueMode = 0; // Controls the flow of #active-clue element while selecting a clue, displaying the question of selected clue, and displaying the answer to the question.
@@ -85,13 +17,7 @@ let isPlayButtonClickable = true; // Only clickable when the game haven't starte
 
 $("#play").on("click", handleClickOfPlay);
 
-/**
- * Manages the behavior of the play button (start or restart) when clicked.
- * Sets up the game.
- *
- * Hints:
- * - Sets up the game when the play button is clickable.
- */
+
 function handleClickOfPlay ()
 {
   // todo set the game up if the play button is clickable
@@ -110,13 +36,8 @@ function handleClickOfPlay ()
  */
 async function setupTheGame ()
 {
-  // todo show the spinner while setting up the game
-
-  // todo reset the DOM (table, button text, the end text)
-
-  // todo fetch the game data (categories with clues)
-
-  // todo fill the table
+let res = await axios.get(`https://rithm-jeopardy.herokuapp.com/api/category?
+id=2`)
 }
 
 /**
